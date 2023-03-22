3283f918c4c33716c0bef717420e242514877012
MikeOwusu <125754182+MikeOwusu@users.noreply.github.com>
2023-03-22 22:15:09 +0000

    Create 1-alphabet.c
    Alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <ctype.h>
+#include "main.h"
+/**
+ * print_alphabet - prints alphabet in lower case
+ *
+ * Return: always 0 (success)
+ */
+
+
+void print_alphabet(void)
+{
+	char c, cl;
+
+	c = 'A';
+	cl = tolower(c);
+	for (cl = 'a'; cl <= 'z'; cl++)
+		_putchar(cl);
+	_putchar('\n');
+}

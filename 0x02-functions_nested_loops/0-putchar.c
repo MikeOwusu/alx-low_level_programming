704666ac5790c79d8322b28e5d47f60cd2978a5b
MikeOwusu <owusukwekumichael@gmail.com>
2023-03-21 20:21:34 -0700

    fixing error

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,14 +1,17 @@
 #include <stdio.h>
-
-int main() {
-   char str[] = "_putchar";
+/**
+ * main - entry point
+ *
+ * Return: always 0 (success)
+ */
+int main()
+{
+   char str[]="_putchar";
    int i;
 
    for (i = 0; str[i] != '\0'; i++) {
       putchar(str[i]);
    }
-
    putchar('\n');
-
    return 0;
 }

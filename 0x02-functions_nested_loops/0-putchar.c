a3ccf1865c3090318b8af6a642254958cfcb4a71
MikeOwusu <125754182+MikeOwusu@users.noreply.github.com>
2023-03-22 22:13:06 +0000

    Update 0-putchar.c
    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include "main.h"
 /**
  * main - entry point
  *
@@ -6,12 +7,14 @@
  */
 int main(void)
 {
- char str[] = "_putchar";
- int i;
- for (i = 0; str[i] != '\0'; i++) 
- {
-putchar(str[i]);
- }
-putchar('\n');
-   return 0;
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
 }

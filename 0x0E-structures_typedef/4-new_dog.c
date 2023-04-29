a32ba4d294a4d7fb93420750471321fdf879676f
MikeOwusu <owusukwekumichael@gmail.com>
2023-04-29 16:52:53 -0700

    fix

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -5,7 +5,8 @@
  * new_dog - creates a new dog
  * @name: name of dog
  * @age: age of dog
- * @owner: owner of dog 
+ * @owner: owner of dog
+ *
  * Return: pointer to new dog
  */
 dog_t *new_dog(char *name, float age, char *owner)

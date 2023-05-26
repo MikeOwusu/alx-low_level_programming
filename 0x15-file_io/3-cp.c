3f6ab9221c83ef29036a6fe1dd0c012b0509b2c8
MikeOwusu <owusukwekumichael@gmail.com>
2023-05-25 17:02:51 -0700

    i/o edit

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 
 /**
- * error_file - checks if the files can be opened
+ * error_file - checks if files can be opened
  * @file_from: file_from
  * @file_to: file_to
  * @argv: arguments vector
@@ -21,4 +21,54 @@ void error_file(int file_from, int file_to, char *argv[])
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 		exit(99);
 	}
+}
+
+/**
+ * main - check the code for Alx School students
+ * @argc: number of arguments
+ * @argv: arguments vector
+ * Return: Always 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int file_from, file_to, err_close;
+	ssize_t nchars, xy;
+	char buf[1024];
+
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO, "%s\n", "Usage: cp file_from file_to");
+		exit(97);
+	}
+
+	file_from = open(argv[1], O_RDONLY);
+	file_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC | O_APPEND, 0664);
+	error_file(file_from, file_to, argv);
+
+	nchars = 1024;
+	while (nchars == 1024)
+	{
+		nchars = read(file_from, buf, 1024);
+		if (nchars == -1)
+			error_file(-1, 0, argv);
+
+		xy = write(file_to, buf, nchars);
+		if (xy == -1)
+			error_file(0, -1, argv);
+	}
+	err_close = close(file_from);
+	if (err_close == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from);
+		exit(100);
+	}
+
+	err_close = close(file_to);
+	if (err_close == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from);
+		exit(100);
+	}
+	return (0);
 }
